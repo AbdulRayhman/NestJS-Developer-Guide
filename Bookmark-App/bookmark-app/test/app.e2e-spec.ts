b7c3ca2dc294e056b0327783169e5fbc62ceed83
Abdul Rehman <s.abderrehman@gmail.com>
2024-03-07 00:32:20 +0500

    automate the script for testing, add dotenv-cli for different env

--- Bookmark-App/bookmark-app/test/app.e2e-spec.ts
+++ Bookmark-App/bookmark-app/test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-
+import { spec } from 'pactum';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
