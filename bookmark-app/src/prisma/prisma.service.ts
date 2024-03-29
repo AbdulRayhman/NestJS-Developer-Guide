332239c109b7215559a04c6df6092466335f1387
Abdul Rehman <s.abderrehman@gmail.com>
2024-03-29 18:19:27 +0500

    Add prod db

--- bookmark-app/src/prisma/prisma.service.ts
+++ bookmark-app/src/prisma/prisma.service.ts
@@ -5,7 +5,15 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(config: ConfigService) {
-    super({ datasources: { db: { url: config.get('DATABASE_URL') } } });
+    super({
+      datasources: {
+        db: {
+          url: config.get(
+            'postgresql://nest_owner:s2wydTkhbPL6@ep-damp-tooth-a2ymrfar.eu-central-1.aws.neon.tech/nest?sslmode=require',
+          ),
+        },
+      },
+    });
   }
 
   cleanDB() {
